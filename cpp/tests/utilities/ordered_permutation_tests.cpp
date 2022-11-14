9a6ce35e6f0238f43b8335ae7f9d57fd71641cea
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2022-11-14 17:03:34 +0100

    Added OrderedPermutationIterator and associated tests.

---
+++ cpp/tests/utilities/ordered_permutation_tests.cpp
@@ -0,0 +1,197 @@
+/**
+ * ordered_combination_tests.cpp
+ * 
+ * Copyright (c) 2022 Austrian Academy of Sciences
+ */
+
+#include "gtest/gtest.h"
+
+#include "utilities/ordered_permutation.h"
+
+namespace NPATK::Tests {
+
+    TEST(OrderedPermutation, Gen2C1) {
+        OrderedPermutationIterator<int> opi{2, 1};
+        OrderedPermutationIterator<int> opi_end{2, 1, true};
+
+        ASSERT_NE(opi, opi_end);
+        ASSERT_EQ(opi->size(), 1);
+
+        EXPECT_EQ(opi[0], 0);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        ++opi;
+
+        ASSERT_EQ(opi, opi_end);
+    }
+
+    TEST(OrderedPermutation, Gen2C2) {
+        OrderedPermutationIterator<int> opi{2, 2};
+        OrderedPermutationIterator<int> opi_end{2, 2, true};
+
+        ASSERT_NE(opi, opi_end);
+        ASSERT_EQ(opi->size(), 2);
+
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 1);
+        ++opi;
+
+        ASSERT_EQ(opi, opi_end);
+    }
+
+    TEST(OrderedPermutation, Gen2C3) {
+        OrderedPermutationIterator<int> opi{2, 3};
+        OrderedPermutationIterator<int> opi_end{2, 3, true};
+
+        ASSERT_NE(opi, opi_end);
+        ASSERT_EQ(opi->size(), 3);
+
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        EXPECT_EQ(opi[2], 0);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_EQ(opi, opi_end);
+    }
+
+    TEST(OrderedPermutation, Gen3C2) {
+        OrderedPermutationIterator<int> opi{3, 2};
+        OrderedPermutationIterator<int> opi_end{3, 2, true};
+
+        ASSERT_NE(opi, opi_end);
+        ASSERT_EQ(opi->size(), 2);
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 2);
+        EXPECT_EQ(opi[1], 2);
+        ++opi;
+
+        ASSERT_EQ(opi, opi_end);
+    }
+
+    TEST(OrderedPermutation, Gen3C3) {
+        OrderedPermutationIterator<int> opi{3, 3};
+        OrderedPermutationIterator<int> opi_end{3, 3, true};
+
+        ASSERT_NE(opi, opi_end);
+        ASSERT_EQ(opi->size(), 3);
+
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        EXPECT_EQ(opi[2], 0);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 0);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 0);
+        EXPECT_EQ(opi[1], 2);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 1);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 1);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 1);
+        EXPECT_EQ(opi[1], 2);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+        ASSERT_NE(opi, opi_end);
+        EXPECT_EQ(opi[0], 2);
+        EXPECT_EQ(opi[1], 2);
+        EXPECT_EQ(opi[2], 2);
+        ++opi;
+
+
+        ASSERT_EQ(opi, opi_end);
+    }
+
+}
