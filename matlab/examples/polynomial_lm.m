642820018174c8691fc3eba1fdbc7e3c8c3bb9da
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2023-07-18 17:08:23 +0200

    Rename nonhermitian_lm.m to polynomial_lm.m, and remove redundant three_party.m.

--- matlab/examples/polynomial_lm.m
+++ matlab/examples/polynomial_lm.m
@@ -1,20 +1,23 @@
-%% Clear
-clear
-clear mtk;
+%% Example: polynomial_lm.m
+% Demonstrates usage of polynomial localizing matrices.
+% Analytical result: 1
+%
 
 %% Params
-mm_level = 1;
+mm_level = 2;
 lm_level = max(mm_level - 1, 0);
 
 %% Create setting
-setting = AlgebraicScenario(["x1", "x2"], {{[1, 1], [1]}}, true);
-setting.Complete(4);
+setting = AlgebraicScenario(["x1", "x2"], ...
+                            'rules', {{[1, 1], [1]}}, ...
+                            'hermitian', true);
 
+%% Create polynomials
 [x1, x2] = setting.getAll();
 re_x1x2 = (x1*x2 + x2*x1)*0.5;
 im_x1x2 = (x1*x2 - x2*x1)*(-0.5i);
 
-%% Make matrices 
+%% Make operator matrices 
 mm = setting.MomentMatrix(mm_level);
 lm1 = re_x1x2.LocalizingMatrix(lm_level);
 lm2 = im_x1x2.LocalizingMatrix(lm_level);
@@ -23,16 +26,16 @@ lm2 = im_x1x2.LocalizingMatrix(lm_level);
 cvx_begin sdp
 
     % Declare basis variables a (real) b (imaginary)
-    mm.cvxVars('a', 'b');
+    setting.cvxVars('a', 'b');
     
     % Compose moment matrix from these basis variables
-    M = mm.cvxComplexMatrix(a, b);
+    M = mm.cvx(a, b);
     
     % Compose localizing matrix from these basis variables
-    L1 = lm1.cvxComplexMatrix(a, b);
+    L1 = lm1.cvx(a, b);
     
     % Compose localizing matrix from these basis variables
-    L2 = lm2.cvxComplexMatrix(a, b);
+    L2 = lm2.cvx(a, b);
     
     % Constraints
     a(1) == 1;
