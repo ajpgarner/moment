724b2fb554977a399b90cc96ec0c0933381bfbc2
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2023-07-21 16:24:39 +0200

    Removed argument blocks from MTKUtil functions.

--- matlab/functions/+MTKUtil/alphabetic_index.m
+++ matlab/functions/+MTKUtil/alphabetic_index.m
@@ -1,11 +1,15 @@
 function [theString] = alphabetic_index(theIndex, isUpper, isZeroIndex)
 % ALPHABETIC_INDEX Convert index/indices from numbers to letters.
 % The letters are assigned in shortlex (excel-like) order: A-Z, AA-ZZ, etc.
-    arguments
-        theIndex (:,:) uint64
-        isUpper (1,1) logical = true
-        isZeroIndex (1,1) logical = false
-    end
+
+	% Validate
+	assert(nargin>=1);
+	if (nargin<2)
+		isUpper = true;
+	end	
+	if (nargin<3)
+		isZeroIndex = false;
+	end
 
     flags = cell(1,0);
     if isUpper
--- matlab/functions/+MTKUtil/cell_mask.m
+++ matlab/functions/+MTKUtil/cell_mask.m
@@ -1,9 +1,15 @@
 function output = cell_mask(cell_array, mask, assign_value)
 % CELL_MASK Apply value across cell array at logical indices
+
+	assert(nargin>=2);
+	if (nargin < 3)
+		assign_value = [];
+	end
+
     assert(isequal(size(cell_array), size(mask)));
     output = cell_array;
-    if any(mask(:))
-        output(mask) = deal(repelem({assign_value}, sum(mask(:))));
+    if any(mask, 'all')
+        output(mask) = deal(repelem({assign_value}, sum(mask, 'all')));    
     end
 end
 
