5beb4ec44e7096d9b2ff1d5338407cae2a773397
Andrew Garner <6692740+ajpgarner@users.noreply.github.com>
2023-07-24 09:19:29 +0200

    Adjust mtk_tests.m to search recursively.

--- matlab/mtk_tests.m
+++ matlab/mtk_tests.m
@@ -1,9 +1,26 @@
-clear mtk
+%% mtk_tests.m
+% Runs MATLAB unit tests.
 
-[test_dir, ~, ~] = fileparts(mfilename('fullpath'));
-test_dir = [test_dir, '\tests\'];
-test_files = {dir([test_dir, '*Test.m']).name};
-test_files = cellfun(@(str)[test_dir, str], test_files, 'uni', false);
+test_dir = get_test_folder();
+test_files = find_unit_tests(test_dir);
+fprintf("Found %d unit test files...\n", numel(test_files));
 
+old_path = path();
+addpath(test_dir);
 summary = runtests(test_files);
+path(old_path);
+
 disp(summary);
+
+function test_dir = get_test_folder()
+    [test_dir, ~, ~] = fileparts(mfilename('fullpath'));
+    test_dir = [test_dir, '\tests\'];
+end
+
+function files = find_unit_tests(test_dir)    
+    test_search_struct = dir([test_dir, '**\*Test.m']);
+    names = {test_search_struct.name};
+    folders = {test_search_struct.folder};
+    files = cellfun(@(a,b) [a, '\', b], folders, names, ...
+                    'UniformOutput', false);
+end
